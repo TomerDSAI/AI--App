6fb8a73f2ab5744714a2fa0b806cfa2fbe5ae0e6
tomerx2389x <tomerm2389@gmail.com>
2026-04-13 12:40:32 +0300

    eval

--- functions.py
+++ functions.py
@@ -392,14 +392,14 @@ def final_evaluation(model,X_train,X_test,y_train ,y_test,metric):
     train_score = accuracy_score(y_train,y_train_pred)
     return(f"the final {metric} on the test set is {score*100}% and on the training set is {train_score*100}%")
   elif metric == "recall":
-    score = recall_score(y_test,y_pred)
+    score = recall_score(y_test,y_pred , average = "weighted")
     train_score = recall_score(y_train,y_train_pred)
     return(f"the final {metric} on the test set is {score*100}% and on the training set is {train_score*100}%")
   elif metric == "precision":
-    score = precision_score(y_test,y_pred)
+    score = precision_score(y_test,y_pred , average = "weighted" )
     train_score = precision_score(y_train,y_train_pred)
     return(f"the final {metric} on the test set is {score*100}% and on the training set is {train_score*100}%")
   elif metric == "f1":
-    score = f1_score(y_test,y_pred)
+    score = f1_score(y_test,y_pred , average = "weighted")
     train_score = f1_score(y_train,y_train_pred)
     return(f"the final {metric} on the test set is {score*100}% and on the training set is {train_score*100}%")
